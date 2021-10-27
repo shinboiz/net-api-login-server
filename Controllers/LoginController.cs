2b457d5fa5a91a2018158feb76b3683d47ec1da9
TrungDo <trungdv1987@gmail.com>
2021-10-27 11:57:36 +0700

    Log file path of writing

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -50,11 +50,14 @@ namespace LoginApi.Controllers {
         }
 
         private async Task WriteFile(string content, string fileName) {
+
             // Set a variable to the Documents path.
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
     
             // TODO verify directory exists, Name is not null, Path is not null, Body is not null
             string fullPath = System.IO.Path.Combine(docPath, fileName);
+
+            _logger.LogInformation(string.Format("Writing data to file = {0}", fullPath));
 
             // Simplest way to write to file
             await System.IO.File.WriteAllTextAsync(fullPath, content);
